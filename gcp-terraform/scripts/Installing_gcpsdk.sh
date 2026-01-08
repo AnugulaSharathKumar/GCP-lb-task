519faeed38116ec6eb8a0cb37f8917714912bfc3
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2026-01-08 14:14:13 +0530

    Update Installing_gcpsdk.sh

--- gcp-terraform/scripts/Installing_gcpsdk.sh
+++ gcp-terraform/scripts/Installing_gcpsdk.sh
@@ -10,16 +10,38 @@ if command -v gcloud >/dev/null 2>&1; then
     gcloud version
 else
     echo "❌ Google Cloud SDK not found. Installing..."
-    echo
-
     #######################################################################
-    echo "Updating the system"
-    sudo apt update -y
-    sudo apt install -y apt-transport-https ca-certificates gnupg curl
+echo " Updating the system"
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates gnupg curl
 
-    #######################################################################
-    echo "Adding the Google Cloud GPG key"
-    curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-    sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+###########################################################################
+echo "Adding the Google Cloud GPG key"
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 
-    ##########################
+###########################################################################
+echo "adding the Google Cloud SDK repository"
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
+https://packages.cloud.google.com/apt cloud-sdk main" | \
+sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+
+
+###########################################################################
+echo "Installing the gcloud in codespace"
+sudo apt update
+sudo apt install -y google-cloud-cli
+
+###########################################################################
+echo "Verifying the gcp is installed or not"
+gcloud version
+
+###########################################################################
+echo "Enabling the application default credentials"
+gcloud auth application-default login
+
+echo "Checking the adc is ready or not"
+ls ~/.config/gcloud/application_default_credentials.json
+
+    echo "✅ Google Cloud SDK installation completed."
+fi
