1447b838c928ef220cb3509a9d9fa4edc2ad1fb1
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2026-01-08 06:41:34 +0000

    added

--- gcp-terraform/scripts/Installing_gcpsdk.sh
+++ gcp-terraform/scripts/Installing_gcpsdk.sh
@@ -1,32 +1,25 @@
-#######################################################################
-echo " Updating the system"
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates gnupg curl
+#!/bin/bash
+set -e
 
-###########################################################################
-echo "Adding the Google Cloud GPG key"
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "========================================"
+echo "Checking Google Cloud SDK installation"
+echo "========================================"
 
-###########################################################################
-echo "adding the Google Cloud SDK repository"
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
-https://packages.cloud.google.com/apt cloud-sdk main" | \
-sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+if command -v gcloud >/dev/null 2>&1; then
+    echo "✅ Google Cloud SDK is already installed."
+    gcloud version
+else
+    echo "❌ Google Cloud SDK not found. Installing..."
+    echo
 
+    #######################################################################
+    echo "Updating the system"
+    sudo apt update -y
+    sudo apt install -y apt-transport-https ca-certificates gnupg curl
 
-###########################################################################
-echo "Installing the gcloud in codespace"
-sudo apt update
-sudo apt install -y google-cloud-cli
+    #######################################################################
+    echo "Adding the Google Cloud GPG key"
+    curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+    sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 
-###########################################################################
-echo "Verifying the gcp is installed or not"
-gcloud version
-
-###########################################################################
-echo "Enabling the application default credentials"
-gcloud auth application-default login
-
-echo "Checking the adc is ready or not"
-ls ~/.config/gcloud/application_default_credentials.json
+    ##########################
